b51518cd701b946d7c54f7f2ced8c233a1bbc90f
ivethG <ivethglez80@gmail.com>
2024-04-17 17:31:17 -0300

    new

--- src/db.js
+++ src/db.js
@@ -6,16 +6,16 @@ require ("dotenv").config();
 const {DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_DEPLOY}=process.env;
 
 // ACTIVA PARA LOCAL
-const sequelize = new Sequelize(
-    `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/fundGoal`,
-    {logging:false} //esto es para que no llene la consola
-);
-
-//ACTIVA PARA PRODUCCION
 // const sequelize = new Sequelize(
-//     DB_DEPLOY,
+//     `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/fundGoal`,
 //     {logging:false} //esto es para que no llene la consola
 // );
+
+//ACTIVA PARA PRODUCCION
+const sequelize = new Sequelize(
+    DB_DEPLOY,
+    {logging:false} //esto es para que no llene la consola
+);
 
 
 FundGoalModel(sequelize);
